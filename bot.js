a6404b5e7d1741331269d05b13417a4cfcad8456
Ruslan Abramov <96062993+Impossible2495@users.noreply.github.com>
2024-04-01 19:03:43 +0300

    Работает и на целевом сайте, случайно уходит в поиск

--- bot.js
+++ bot.js
@@ -1,20 +1,23 @@
 // ==UserScript==
 // @name         First Bot
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Bot for Bing
 // @author       Abramov Ruslan
 // @match        https://www.bing.com/*
+// @match        https://napli.ru/*
 // @grant        none
 // ==/UserScript==
 
 let searchInput = document.getElementsByName("q")[0];
 let links = document.links;
 let searchBtn = document.getElementById("search_icon");
-let keywords = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
+let keywords = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google",
+                "Отключение редакций и ревизий", "Webpack, Parcel и Rollup",
+                "Вывод произвольных типов записей и полей"];
 let keyword = keywords[getRandom(0, keywords.length)];
 
-
+//Работаем на главной странице поисковика
 if (searchBtn !== null) {
   let i = 0;
   let timerId = setInterval(() => {
@@ -25,14 +28,47 @@ if (searchBtn !== null) {
       searchBtn.click();
     }
   }, 500);
-} else {
+}
+//Работаем на целевом сайте
+else if (location.hostname == "napli.ru") {
+  console.log("Мы на napli.ru!!!");
+
+  setInterval(()=> {
+    let index = getRandom(0, links.length);
+    let localLink = links[index];
+
+    if (getRandom(0, 101) > 75) {
+      location.href = "https://www.bing.com/";
+    }
+
+    if (localLink.href.includes("napli.ru")) {
+      localLink.click();
+    }
+  }, getRandom(2000, 4000))
+
+}
+//Работаем на страницах поисковой выдачи
+else if (document.querySelector(".b_scopebar") !== null) {
+  let nextBingPage = true;
   for (let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf("napli.ru") != -1) {
       let link = links[i];
+      nextBingPage = false;
       console.log("Нашел строку " + link);
-      link.click();
+      setTimeout(()=> {
+        link.click();
+      }, getRandom(3000, 5000))
       break;
     }
+  }
+  if (document.querySelector(".sb_pagS").innerText == "5") {
+    nextBingPage = false;
+    location.href = "https://www.bing.com/";
+  }
+  if (nextBingPage) {
+    setTimeout(()=>{
+      document.querySelector(".sb_pagN").click();
+    }, getRandom(2000, 4000))
   }
 }
 
